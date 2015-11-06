44e3105e57907f1740d8b06b7c0a9963a59a0a33
Zack Craig <zack.e.craig@gmail.com>
2015-11-06 09:57:10 -0500

    added reset function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
 	$(".ryu").mouseenter(function() {
-		$(".ryu-still").hide();
+		reset();
 		$(".ryu-ready").show();
-		$(".ryu-cool").hide();
 	})
 	.mouseleave(function() {
-		$(".ryu-ready").hide();
+		reset();
 		$(".ryu-still").show();
-		$(".ryu-cool").hide();
 	})
 	.mousedown(function() {
 		playHadouken();
-		$(".ryu-ready").hide();
+		reset();
 		$(".ryu-throwing").show();
-		$(".ryu-cool").hide();
 		$(".hadouken").finish().show().animate(
 			{"left": "1020px"},
 			500,
@@ -24,23 +21,21 @@ $(document).ready(function() {
 		);
 	})
 	.mouseup(function() {
-		$(".ryu-throwing").hide();
+		reset();
 		$(".ryu-ready").show();
 		// ryu goes back to his ready position
 	})
 
 	$("body").keydown(function(event) {
 		if(event.which == 88) {			
-			$(".ryu-still").hide();
-			$(".ryu-ready").hide();
+			reset();
 			$(".ryu-cool").show();
 		}
 	})
 	$("body").keyup(function(event) {
 		if(event.which == 88) {
+			reset();
 			$(".ryu-still").show();
-			$(".ryu-ready").hide();
-			$(".ryu-cool").hide();
 		}
 	});
 });
@@ -49,4 +44,11 @@ function playHadouken() {
 	$("#hadouken-sound")[0].volume=0.5;
 	$("#hadouken-sound")[0].load();
 	$("#hadouken-sound")[0].play();
+}
+
+function reset() {
+	$(".ryu-still").hide();
+	$(".ryu-ready").hide();
+	$(".ryu-cool").hide();
+	$(".ryu-throwing").hide();
 }
